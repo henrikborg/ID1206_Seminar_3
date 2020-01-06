fedbebde57386782c408c703d20e0a9fb4cd07f1
Henrik Borg <hborg76@gmail.com>
2020-01-06 04:34:38 +0100

    ADD The three first tests Easy to select betwewn them

--- test.c
+++ test.c
@@ -4,7 +4,40 @@
 static green_cond_t cond_queue;
 green_cond_t cond;
 
+/* 2.4 */
+void *test_2(void *arg) {
+  int i = *(int*)arg;
+  int loop = 4;
+
+  while(loop > 0) {
+    printf("thread %d: %d\n", i, loop);
+    loop--;
+    green_yield();
+  }
+}
+
+/* 3 */
+int flag = 0;
+
 void *test(void *arg) {
+  int id = *(int*)arg;
+  int loop = 4;
+
+  while(loop > 0) {
+    if(flag == id) {
+      printf("thread %d: %d\n", id, loop);
+      loop--;
+      flag = (id + 1) % 4;
+      green_cond_signal(&cond);
+    } else {
+      flag = (id + 1) % 4;
+      green_cond_wait(&cond);
+    }
+  }
+}
+
+/* 4 */
+void *test_4(void *arg) {
   int i = *(int*)arg;
   int loop = 4;
   int flag = 0;
@@ -12,21 +45,21 @@ void *test(void *arg) {
   int counter = 0;
 
   while(loop > 0) {
-    while(1) {
+    //while(1) {
       if(flag == id) {
         printf("thread %d: %d\n", id, loop);
         break;
-        //green_yield();
+        green_yield();
       } else {
         printf("Suspend on cond %d, flag is %d, loop is %d\n", id, flag, loop);
-        green_cond_wait(&cond);
+        //green_cond_wait(&cond);
       }
-    }
+    //}
     counter++;
     loop--;
     flag = (flag + 1) % 4;
     printf("Signal on cond %d, flag is %d, loop is %d\n", id, flag, loop);
-    green_cond_signal(&cond);
+    //green_cond_signal(&cond);
   }
 }
 
@@ -40,14 +73,14 @@ int main() {
   green_cond_init(&cond_queue);
 
   green_create(&g0, test, &a0);
-  green_create(&g1, test, &a1);
-  green_create(&g2, test, &a2);
-  green_create(&g3, test, &a3);
+  //green_create(&g1, test, &a1);
+  //green_create(&g2, test, &a2);
+  //green_create(&g3, test, &a3);
 
   green_join(&g0, NULL);
-  green_join(&g1, NULL);
-  green_join(&g2, NULL);
-  green_join(&g3, NULL);
+  //green_join(&g1, NULL);
+  //green_join(&g2, NULL);
+  //green_join(&g3, NULL);
 
   printf("done\n");
   return 0;
