dac9a78705c06304dcb0d63ac18e7e281d2c926d
Henrik Borg <hborg76@gmail.com>
2020-01-06 02:14:51 +0100

    UPDATE Function test in main.c

--- test.c
+++ test.c
@@ -12,34 +12,42 @@ void *test(void *arg) {
   int counter = 0;
 
   while(loop > 0) {
-    if(flag == id) {
-      printf("thread %d: %d\n", id, loop);
-      //green_yield();
-    } else {
-      //flag = (flag + 1) % 2;
-      printf("Suspend on cond %d, flag is %d, loop is %d\n", id, flag, loop);
-      green_cond_wait(&cond);
+    while(1) {
+      if(flag == id) {
+        printf("thread %d: %d\n", id, loop);
+        break;
+        //green_yield();
+      } else {
+        printf("Suspend on cond %d, flag is %d, loop is %d\n", id, flag, loop);
+        green_cond_wait(&cond);
+      }
     }
     counter++;
     loop--;
-    flag = (id + 1) % 2;
+    flag = (flag + 1) % 4;
     printf("Signal on cond %d, flag is %d, loop is %d\n", id, flag, loop);
     green_cond_signal(&cond);
   }
 }
 
 int main() {
-  green_t g0, g1;
+  green_t g0, g1, g2, g3;
   int a0 = 0;
-  int a1 = 0;
+  int a1 = 1;
+  int a2 = 2;
+  int a3 = 3;
 
   green_cond_init(&cond_queue);
 
   green_create(&g0, test, &a0);
   green_create(&g1, test, &a1);
+  green_create(&g2, test, &a2);
+  green_create(&g3, test, &a3);
 
   green_join(&g0, NULL);
   green_join(&g1, NULL);
+  green_join(&g2, NULL);
+  green_join(&g3, NULL);
 
   printf("done\n");
   return 0;
