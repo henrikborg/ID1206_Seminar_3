6b8e6362d0f21c1da9cdc3d869d83c69b4f550f0
Henrik Borg <hborg76@gmail.com>
2020-01-08 16:27:09 +0100

    MOD Added embryo for test for task 5

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include "green.h"
 
 green_cond_t cond;
+green_cond_t cond2;
 
 /* TASK 2.4 */
 #ifdef TASK2
@@ -74,20 +75,48 @@ void *test(void *arg) {
     counter++;
     loop--;
     flag = (flag + 1) % THREADS;
-    printf("Signal on cond %d, flag is %d, loop is %d\n", id, flag, loop);
+    printf("Signal  on cond %d, flag is %d, loop is %d\n", id, flag, loop);
+    green_cond_signal(&cond);
+  }
+}
+#endif
+
+#ifdef TASK5
+int flag = 0;
+
+void *test(void *arg) {
+  int i = *(int*)arg;
+  int loop = 4;
+  //int flag = 0;
+  int id = *(int*)arg;
+  int counter = 0;
+
+  while(loop > 0) {
+    while(flag != id) {
+      printf("Suspend on cond %d, flag is %d, loop is %d\n", id, flag, loop);
+      green_cond_wait(&cond);
+    }
+    counter++;
+    loop--;
+    flag = (id + 1) % THREADS;
+    printf("Signal  on cond %d, flag is %d, loop is %d\n", id, flag, loop);
     green_cond_signal(&cond);
   }
 }
 #endif
 
 int main() {
-  green_t g0, g1, g2, g3;
-  green_t* g_vector[] = {&g0, &g1, &g2, &g3};
+  green_t g0, g1, g2, g3, g4, g5, g6, g7;
+  //green_t* g_vector[] = {&g0, &g1, &g2, &g3};
   int a0 = 0;
   int a1 = 1;
   int a2 = 2;
   int a3 = 3;
-  int* a_vector[] = {&a0, &a1, &a2, &a3};
+  int a4 = 4;
+  int a5 = 5;
+  int a6 = 6;
+  int a7 = 7;
+  //int* a_vector[] = {&a0, &a1, &a2, &a3};
 
   green_cond_init(&cond);
   green_enable_timer();
@@ -100,6 +129,10 @@ int main() {
   if(i++ < THREADS) green_create(&g1, test, &a1);
   if(i++ < THREADS) green_create(&g2, test, &a2);
   if(i++ < THREADS) green_create(&g3, test, &a3);
+  if(i++ < THREADS) green_create(&g4, test, &a4);
+  if(i++ < THREADS) green_create(&g5, test, &a5);
+  if(i++ < THREADS) green_create(&g6, test, &a6);
+  if(i++ < THREADS) green_create(&g7, test, &a7);
 
   /*for(int i = 0; i < THREADS; i++) {
     green_join(g_vector[i], NULL);
@@ -109,6 +142,10 @@ int main() {
   if(i++ < THREADS) green_join(&g1, NULL);
   if(i++ < THREADS) green_join(&g2, NULL);
   if(i++ < THREADS) green_join(&g3, NULL);
+  if(i++ < THREADS) green_join(&g4, NULL);
+  if(i++ < THREADS) green_join(&g5, NULL);
+  if(i++ < THREADS) green_join(&g6, NULL);
+  if(i++ < THREADS) green_join(&g7, NULL);
 
 #ifdef TASK2
   printf("TASK 2 ");
